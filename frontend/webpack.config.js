f269c4fa79e2102b2ebb19bebafcbdb61df7fd39
MHabibiWasi <habibiunyila@gmail.com>
2023-04-07 21:29:08 +0700

    mengintegrasikan tailwind css

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -16,6 +16,10 @@ module.exports = {
         loader: "ts-loader",
         exclude: /node_modules/,
       },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader", "postcss-loader"],
+      },
     ],
   },
   mode: "development",
