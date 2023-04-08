4c28880824a6f00d927ec41663c81eb3f56ca0ed
MHabibiWasi <muhammad.120280027@student.itera.ac.id>
2023-04-08 09:56:15 +0700

    mengubah render menjadi createRoot

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,17 +1,18 @@
 import React, { useState } from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import "./index.css";
 
 const App = () => {
   const [teks, setTeks] = useState("");
   const [value, setValue] = useState(0);
   return (
-    <div>
+    <>
       <h1>Hello, {teks}!</h1>
       <input type="text" onChange={(e) => setTeks(e.target.value)} />
       <button onClick={() => setValue((prev) => prev + 1)}>tambahkan saya</button>
-    </div>
+    </>
   );
 };
-
-ReactDOM.render(<App />, document.getElementById("root"));
+// ReactDOM.render(<App />, document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById("root")!);
+root.render(<App />);
