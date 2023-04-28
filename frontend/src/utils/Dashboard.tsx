f41a65bae285cb291f8ea991c1b470fe610f1871
MHabibiWasi <muhammad.120280027@student.itera.ac.id>
2023-04-28 22:02:47 +0700

    Ngapus redux gajelas, mindahin context dashboard ke dashboard component

--- frontend/src/utils/Dashboard.tsx
+++ frontend/src/utils/Dashboard.tsx
@@ -1,6 +1,6 @@
 import React, { useContext } from "react";
 import DashboardHeader from "../components/Navbar/DashboardHeader";
-import { DashboardContext } from "../context/DashboardContext";
+import { DashboardContext, DashboardProvider } from "../context/DashboardContext";
 
 interface MyComponentProps {
   component: React.ReactNode[];
@@ -10,10 +10,12 @@ const Dashboard: React.FC<MyComponentProps> = ({ component }) => {
   const { sidebarActive } = useContext(DashboardContext);
   return (
     <div className="">
-      {component[1]}
-      <div className={`${sidebarActive ? `ml-64` : `ml-16`} transition-all duration-300`}>
-        <DashboardHeader component={component[0]} />
-      </div>
+      <DashboardProvider>
+        {component[1]}
+        <div className={`${sidebarActive ? `ml-64` : `ml-16`} transition-all duration-300`}>
+          <DashboardHeader component={component[0]} />
+        </div>
+      </DashboardProvider>
     </div>
   );
 };
